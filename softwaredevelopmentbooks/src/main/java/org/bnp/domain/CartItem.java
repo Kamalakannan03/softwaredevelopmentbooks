ac3901df9f55b3ded6042ad2ea2bcaf1f05a78f8
kamalakannan03 <kamalkannan3592@gmail.com>
2025-03-11 23:19:26 +0530

    test: added failed test case to CartService, implement: added actual code for Book and CartItem

--- softwaredevelopmentbooks/src/main/java/org/bnp/domain/CartItem.java
+++ softwaredevelopmentbooks/src/main/java/org/bnp/domain/CartItem.java
@@ -3,7 +3,9 @@ package org.bnp.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
+@Data
+@AllArgsConstructor
 public class CartItem {
-
+    private Book book;
+    private int quantity;
 }
--- softwaredevelopmentbooks/src/test/java/org/bnp/service/CartServiceTest.java
+++ softwaredevelopmentbooks/src/test/java/org/bnp/service/CartServiceTest.java
@@ -1,11 +1,17 @@
 package org.bnp.service;
 
+
 import org.bnp.domain.Book;
 import org.bnp.domain.CartItem;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class CartServiceTest {
-
+    @Test
+    public void testAddBookToCart() {
+        CartService cart = new CartService(totalPrice -> 50.0);
+        Book book = new Book("Clean Code");
+        cart.add(book);
+        assertEquals(50, cart.getTotalPrice());
+    }
 }
