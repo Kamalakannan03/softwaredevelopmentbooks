a0d1e934cfcf6826df0756778a9c2d6121c81427
kamalakannan03 <kamalkannan3592@gmail.com>
2025-03-12 00:38:05 +0530

    test: added failed test cases to check the quantity increase, implement: added actual code to do add operation

--- softwaredevelopmentbooks/src/main/java/org/bnp/service/CartService.java
+++ softwaredevelopmentbooks/src/main/java/org/bnp/service/CartService.java
@@ -1,19 +1,29 @@
 package org.bnp.service;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.bnp.domain.Book;
 import org.bnp.domain.CartItem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+@Getter
 @AllArgsConstructor
 public class CartService {
     private final List<CartItem> cartItemList = new ArrayList<>();
     private final PriceCalculator priceCalculator;
 
-    public void add(Book book){
-        cartItemList.add(new CartItem(book,1));
+    public void add(Book book) {
+        cartItemList.stream()
+                .filter(item -> item.getBook().equals(book))
+                .findFirst()
+                .ifPresentOrElse(
+                        item -> item.setQuantity(item.getQuantity() + 1),
+                        () -> cartItemList.add(new CartItem(book, 1))
+                );
     }
+
     public double getTotalPrice(){
         return priceCalculator.calculate(cartItemList);
     }
