eafc504d6fc0f47d0b608486a87ed5d975b70b7d
kamalakannan03 <kamalkannan3592@gmail.com>
2025-03-12 10:39:22 +0530

    implement: added optimizer class

---
+++ softwaredevelopmentbooks/src/main/java/org/bnp/service/GroupedBookOptimizer.java
@@ -0,0 +1,15 @@
+package org.bnp.service;
+
+import org.bnp.domain.CartItem;
+import org.bnp.model.GroupedBook;
+
+import java.util.List;
+
+public class GroupedBookOptimizer {
+    public GroupedBookOptimizer(GroupedBookDiscountCalculatorService groupedBookDiscountCalculatorService, GroupedBooksGeneratorService groupedBooksGeneratorService) {
+    }
+
+    public List<GroupedBook> getUniqueGroupedBooksWithMaxDiscount(List<CartItem> list) {
+        return null;
+    }
+}
